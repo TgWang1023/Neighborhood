c6e640cdcaed4eda8233029ca497daf4d793d8e7
Tiange Wang <wtgkevin@gmail.com>
2018-09-25 16:56:12 -0700

    init main page

--- Neighborhood/ShareViewController.swift
+++ Neighborhood/ShareViewController.swift
@@ -13,6 +13,22 @@ class ShareViewController: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
     
+    @IBAction func postItemForUse(_ sender: UIButton) {
+        performSegue(withIdentifier: "PostSegue", sender: nil)
+    }
+    
+    @IBAction func postItemRequest(_ sender: UIButton) {
+        performSegue(withIdentifier: "PostSegue", sender: nil)
+    }
+    
+    @IBAction func allPostedItems(_ sender: UIButton) {
+        performSegue(withIdentifier: "ItemsSegue", sender: nil)
+    }
+    
+    @IBAction func allItemRequests(_ sender: Any) {
+        performSegue(withIdentifier: "ItemsSegue", sender: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
